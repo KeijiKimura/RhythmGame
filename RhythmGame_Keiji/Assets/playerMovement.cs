c36e147b5f0a947287747be81b0bd6ef204f0d11
KeijiKimura <kimuk170@newschool.edu>
2015-11-04 21:10:50 -0500

    reDo
    yaa

---
+++ RhythmGame_Keiji/Assets/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.Q))
+			transform.position = new Vector3 (-5, 0, 0);
+	
+		if (Input.GetKey (KeyCode.W))
+		transform.position = new Vector3 (0, 0,0);
+
+		if (Input.GetKey (KeyCode.E))
+			transform.position = new Vector3 (5, 0 ,0);
+
+}
+}
